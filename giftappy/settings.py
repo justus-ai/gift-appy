6db1a0f2a073a6c7985eea5dab0d340b6f3466ce
Justus marwa <83753891+justus-ai@users.noreply.github.com>
2026-04-14 09:30:45 +0000

    Fix: move cloudinary_storage before staticfiles in INSTALLED_APPS

--- giftappy/settings.py
+++ giftappy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'home',
     'profiles',
@@ -53,7 +54,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
-    'cloudinary_storage',
     'cloudinary',
 ]
 
